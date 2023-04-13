88b843c97747dda8de5946e8c7bf2ccdd5ddee66
CyberSanyok <sashad92175@gmail.com>
2023-04-13 10:47:13 +0300

    Добавьте файлы проекта.

---
+++ Task_10_PolesUP/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 2;
+        int r = 3;
+        int n = 5;
+        Console.WriteLine(SumN(a,r,n));
+    }
+    public static int SumN(int a, int r,int n)
+    {
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += a + r*i;
+        }
+        return sum;
+    }
+}
